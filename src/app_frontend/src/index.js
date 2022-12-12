44312ec72d6a17816b34f1f6213c957012fbccf9
Verkhohliad <ihor.verkhohliad@gmail.com>
2022-12-12 15:28:19 +0000

    Initial commit.

---
+++ src/app_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { app_backend } from "../../declarations/app_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await app_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});

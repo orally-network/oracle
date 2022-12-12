54494808e8a1b9924aa2da9177071f74fa54e4ce
Verkhohliad <ihor.verkhohliad@gmail.com>
2022-12-12 16:45:30 +0000

    Make http outcall for fetching price inside canister and check the whole flow

--- src/oracle/src/main.rs
+++ src/oracle/src/main.rs
@@ -2,6 +2,13 @@ use candid::candid_method;
 use ic_cdk_macros::{self, update, query};
 use std::str::FromStr;
 use ic_cdk::export::{Principal};
+use serde::{Deserialize, Serialize};
+use ic_cdk::api::management_canister::http_request::{
+    http_request, CanisterHttpRequestArgument, HttpHeader, HttpMethod, HttpResponse, TransformArgs,
+    TransformContext,
+};
+use ic_cdk::api::call::RejectionCode;
+use serde_json::{self, Value};
 
 use ic_web3::transports::ICHttp;
 use ic_web3::Web3;
@@ -12,26 +19,94 @@ use ic_web3::{
     types::{Address, TransactionParameters, BlockId, BlockNumber},
 };
 
-use ic_cdk::api::management_canister::http_request::{
-    http_request, CanisterHttpRequestArgument, HttpHeader, HttpMethod, HttpResponse, TransformArgs,
-    TransformContext,
-};
-
 const URL: &str = "https://eth-goerli.g.alchemy.com/v2/bUH5A9MJ6basJ88Hq85y23Ada8CYSvD4";
 const CHAIN_ID: u64 = 5;
 const KEY_NAME: &str = "dfx_test_key";
 const TOKEN_ABI: &[u8] = include_bytes!("./contracts/icp_price_abi.json");
 const CONTRACT_ADDRESS: &str = "0xCFf00E5f685cCE94Dfc6d1a18200c764f9BCca1f";
+const MAX_RESPONSE_BYTES: u64 = 500_000;
 
 type Result<T, E> = std::result::Result<T, E>;
 
 #[update]
 async fn get_address() -> Result<String, String> {
-    let canister_addr = get_eth_addr(Some(Principal::from_str("r7inp-6aaaa-aaaaa-aaabq-cai").unwrap()), None, KEY_NAME.to_string())
+    let canister_addr = get_eth_addr(None, None, KEY_NAME.to_string())
         .await
         .map_err(|e| format!("get canister eth addr failed: {}", e))?;
 
     Ok(hex::encode(canister_addr))
+}
+
+async fn send_request(host: String, url: String, method: HttpMethod, body: Option<Vec<u8>>) -> Result<String, (RejectionCode, String)> {
+    let mut host_header = host.clone().to_owned();
+    host_header.push_str(":443");
+
+    let request_headers = vec![
+        HttpHeader {
+            name: "Host".to_string(),
+            value: host_header,
+        },
+        HttpHeader {
+            name: "User-Agent".to_string(),
+            value: "oracle_canister".to_string(),
+        },
+    ];
+
+    let request = CanisterHttpRequestArgument {
+        url: url.clone(),
+        method,
+        body,
+        max_response_bytes: Some(MAX_RESPONSE_BYTES),
+        transform: Some(TransformContext::new(transform, vec![])),
+        headers: request_headers,
+    };
+
+    ic_cdk::api::print(format!("Requesting url: {}", url.to_string()));
+
+    match http_request(request).await {
+        Ok((response, )) => {
+            ic_cdk::api::print(format!("Response status: {}", response.status));
+
+            let decoded_body = String::from_utf8(response.body)
+                .expect("Remote service response is not UTF-8 encoded.");
+
+            ic_cdk::api::print(format!("Response body: {}", decoded_body));
+
+            Ok(decoded_body)
+        },
+        Err((code, message)) => {
+            ic_cdk::api::print(format!("Error: {}", message));
+            Err((code, message))
+        }
+    }
+}
+
+#[update(name = "fetch_price")]
+#[candid_method(update, rename = "fetch_price")]
+async fn fetch_price() -> Result<String, String> {
+    let host = "api.pro.coinbase.com".to_string();
+
+    let url = format!("https://{host}/products/ICP-USD/stats");
+    ic_cdk::api::print(url.clone());
+
+    match send_request(host, url, HttpMethod::GET, None).await {
+        Ok(response) => {
+            ic_cdk::api::print(format!("Response from fetch_price: {}", response));
+
+            let response_obj: Value = serde_json::from_str(&response).unwrap();
+
+            ic_cdk::api::print(format!("Price: {}", response_obj["last"]));
+
+            Ok(response_obj["last"].to_string())
+        }
+        Err((code, message)) => {
+            let f_message =
+                format!("The http_request resulted into error. RejectionCode: {code:?}, Error: {message}");
+            ic_cdk::api::print(f_message.clone());
+
+            Err(message)
+        }
+    }
 }
 
 #[update(name = "update_price")]
@@ -52,7 +127,7 @@ async fn update_price() -> Result<String, String> {
         TOKEN_ABI
     ).map_err(|e| format!("init contract failed: {}", e))?;
 
-    let canister_addr = get_eth_addr(Some(Principal::from_str("r7inp-6aaaa-aaaaa-aaabq-cai").unwrap()), None, KEY_NAME.to_string())
+    let canister_addr = get_eth_addr(None, None, KEY_NAME.to_string())
         .await
         .map_err(|e| format!("get canister eth addr failed: {}", e))?;
 
@@ -75,17 +150,11 @@ async fn update_price() -> Result<String, String> {
         op.transaction_type = Some(ic_web3::ethabi::ethereum_types::U64::from(2)) //EIP1559_TX_ID
     });
 
-    // todo: make direct call, not trough another canister
-    let call_result: Result<(Result<String, String>,), _> =
-        ic_cdk::api::call::call(
-            Principal::from_text("ryjl3-tyaaa-aaaaa-aaaba-cai").unwrap(),
-            "fetch_price",
-            ()
-        ).await;
+    let price = fetch_price()
+        .await
+        .map_err(|e| format!("fetch_price error: {}", e))?;
 
-    let price = call_result.unwrap().0.unwrap();
-
-    ic_cdk::api::print(format!("Price from oracle: {}", price));
+    ic_cdk::println!("Price from oracle: {}", price);
 
     let txhash = contract
         .signed_call("set_price", (price,), options, hex::encode(canister_addr), key_info, CHAIN_ID)

665010fd2ae77151c60750f34bd1439bfd61fbd8
Verkhohliad <ihor.verkhohliad@gmail.com>
2022-12-23 14:27:49 +0000

    Make workable setup of fetcher, request batch of endpoints with provided frequency

--- src/oracle/src/fetcher.rs
+++ src/oracle/src/fetcher.rs
@@ -12,6 +12,7 @@ use ic_cdk::{
 };
 use ic_cdk::api::call::RejectionCode;
 use ic_web3::futures::TryFutureExt;
+use futures::future::join_all;
 
 use crate::http::send_request;
 
@@ -27,26 +28,72 @@ pub struct Endpoint {
 pub struct Fetcher {
     pub frequency: u64,
     pub endpoints: Option<Vec<Endpoint>>,
+    pub timer_id: Option<TimerId>,
 }
 
-
 impl Fetcher {
-    pub async fn new(endpoints: Option<Vec<Endpoint>>, frequency: u64) -> Self {
+    pub fn new(endpoints: Option<Vec<Endpoint>>, frequency: u64) -> Self {
         let fetcher = Fetcher {
             frequency,
             endpoints,
+            timer_id: None
         };
-
-        set_timer_interval(
-            Duration::from_secs(frequency),
-            || ic_cdk::spawn(Fetcher::fetch())
-        );
 
         fetcher
     }
 
-    async fn fetch() {
+    pub fn start(mut self) {
+        let timer_id = set_timer_interval(
+            Duration::from_secs(self.frequency),
+            move || ic_cdk::spawn(
+                Fetcher::fetch(
+                    self.endpoints.as_ref().ok_or("No endpoints").unwrap().to_vec()
+                )
+            ),
+        );
+
+        self.timer_id = Some(timer_id);
+    }
+
+    pub fn stop(self) {
+        clear_timer(self.timer_id.unwrap());
+    }
+
+    async fn fetch(endpoints: Vec<Endpoint>) {
         // fetch data from endpoints
+
+        let outputs = endpoints.iter().map(|endpoint| {
+            let url = endpoint.url.clone();
+            let resolver = endpoint.resolver.clone();
+
+            async move {
+                match send_request(url, HttpMethod::GET, None).await {
+                    Ok(body) => {
+                        let body: Value = serde_json::from_str(&body).unwrap();
+
+                        Ok(body[resolver].clone())
+                    },
+                    Err(err) => {
+                        Err(err)
+                    }
+                }
+            }
+        }).collect::<Vec<_>>();
+
+        let outputs = join_all(outputs).await;
+
+        outputs.iter().for_each(|output| {
+            match output {
+                Ok(output) => {
+                    ic_cdk::api::print(format!("Output: {}", output));
+                },
+                Err(err) => {
+                    ic_cdk::api::print(format!("Error: {:?}", err));
+                }
+            }
+        });
+
+        return;
 
         // processing it
 
@@ -57,7 +104,7 @@ impl Fetcher {
 
         let host = "api.pro.coinbase.com".to_string();
 
-        let url = format!("https://{host}/products/ICP-USD/stats");
+        let url = format!("https://api.pro.coinbase.com/products/ICP-USD/stats");
         ic_cdk::api::print(url.clone());
 
         match send_request(url, HttpMethod::GET, None).await {

64f9536dccebc15d0a126acda0fd5a8aa9719faa
Verkhohliad <ihor.verkhohliad@gmail.com>
2023-01-12 13:07:05 +0000

    Add PubSub subscription and notify (send transaction to each subscriber) once data processed from sources

--- src/oracle/src/main.rs
+++ src/oracle/src/main.rs
@@ -23,10 +23,11 @@ mod processing;
 
 use pubsub::Subscription;
 
-const URL: &str = "https://eth-goerli.g.alchemy.com/v2/bUH5A9MJ6basJ88Hq85y23Ada8CYSvD4";
+// todo: make rpc and chain_id settable on oracle creating stage
+const URL: &str = "https://eth-goerli.g.alchemy.com/v2/qrm39CebFg7x-b4I4XhJO4e2AocKXpNx";
 const CHAIN_ID: u64 = 5;
 const KEY_NAME: &str = "dfx_test_key";
-const TOKEN_ABI: &[u8] = include_bytes!("./contracts/icp_price_abi.json");
+const ABI: &[u8] = include_bytes!("./contracts/icp_price_abi.json");
 const CONTRACT_ADDRESS: &str = "0xCFf00E5f685cCE94Dfc6d1a18200c764f9BCca1f";
 
 type Result<T, E> = std::result::Result<T, E>;
@@ -113,59 +114,22 @@ async fn stop_fetcher() -> String {
     "Ok".to_string()
 }
 
-#[update(name = "update_price")]
-#[candid_method(update, rename = "update_price")]
-async fn update_price() -> Result<String, String> {
-    // ecdsa key info
-    let derivation_path = vec![ic_cdk::id().as_slice().to_vec()];
-    let key_info = KeyInfo{ derivation_path: derivation_path, key_name: KEY_NAME.to_string() };
-
-    let w3 = match ICHttp::new(URL, None, None) {
-        Ok(v) => { Web3::new(v) },
-        Err(e) => { return Err(e.to_string()) },
-    };
-    let contract_address = Address::from_str(&CONTRACT_ADDRESS).unwrap();
-    let contract = Contract::from_json(
-        w3.eth(),
+#[update(name = "subscribe")]
+#[candid_method(update, rename = "subscribe")]
+async fn subscribe(contract_address: String) -> String {
+    let subscription = Subscription {
         contract_address,
-        TOKEN_ABI
-    ).map_err(|e| format!("init contract failed: {}", e))?;
+        abi: ABI.to_vec(),
+        method: "set_price".to_string(),
+    };
 
-    let canister_addr = get_eth_addr(None, None, KEY_NAME.to_string())
-        .await
-        .map_err(|e| format!("get canister eth addr failed: {}", e))?;
+    ic_cdk::println!("subscribe: {:?}", subscription);
 
-    ic_cdk::println!("canister_addr: {}", canister_addr);
-
-    // add nonce to options
-    let tx_count = w3.eth()
-        .transaction_count(canister_addr, None)
-        .await
-        .map_err(|e| format!("get tx count error: {}", e))?;
-    // get gas_price
-    let gas_price = w3.eth()
-        .gas_price()
-        .await
-        .map_err(|e| format!("get gas_price error: {}", e))?;
-    // legacy transaction type is still ok
-    let options = Options::with(|op| {
-        op.nonce = Some(tx_count);
-        op.gas_price = Some(gas_price);
-        op.transaction_type = Some(ic_web3::ethabi::ethereum_types::U64::from(2)) //EIP1559_TX_ID
+    SUBSCRIPTIONS.with(|subscriptions| {
+        subscriptions.borrow_mut().push(subscription);
     });
 
-    let price: String = "10".to_string();
-
-    ic_cdk::println!("Price from oracle: {}", price);
-
-    let txhash = contract
-        .signed_call("set_price", (price,), options, hex::encode(canister_addr), key_info, CHAIN_ID)
-        .await
-        .map_err(|e| format!("token transfer failed: {}", e))?;
-
-    ic_cdk::println!("txhash: {}", hex::encode(txhash));
-
-    Ok(format!("{}", hex::encode(txhash)))
+    "Ok".to_string()
 }
 
 fn main() {
--- src/oracle/src/pubsub.rs
+++ src/oracle/src/pubsub.rs
@@ -1,8 +1,77 @@
+use crate::*;
 
 // todo: multi params in the future
+#[derive(Debug, Clone)]
 pub struct Subscription {
-    pub chain_id: String,
-    pub rpc_url: String,
+    pub contract_address: String,
     pub method: String,
     pub abi: Vec<u8>,
+}
+
+pub async fn notify(price: f64) {
+    let subscriptions = SUBSCRIPTIONS.with(|subscriptions| subscriptions.borrow().clone());
+
+    ic_cdk::println!("notify subscribers");
+
+    for subscription in subscriptions.iter() {
+        ic_cdk::println!("notify subscriber: {}", subscription.contract_address);
+
+        update_price(
+            subscription.contract_address.clone(),
+            subscription.method.clone(),
+            &subscription.abi.clone(),
+            price,
+        ).await.expect("TODO: panic message");
+    }
+}
+
+pub async fn update_price(address: String, method: String, abi: &[u8], price: f64) -> Result<String, String> {
+    // ecdsa key info
+    let derivation_path = vec![ic_cdk::id().as_slice().to_vec()];
+    let key_info = KeyInfo{ derivation_path, key_name: KEY_NAME.to_string() };
+
+    let w3 = match ICHttp::new(URL, None, None) {
+        Ok(v) => { Web3::new(v) },
+        Err(e) => { return Err(e.to_string()) },
+    };
+    let contract_address = Address::from_str(&address).unwrap();
+    let contract = Contract::from_json(
+        w3.eth(),
+        contract_address,
+        abi
+    ).map_err(|e| format!("init contract failed: {}", e))?;
+
+    let canister_addr = get_eth_addr(None, None, KEY_NAME.to_string())
+        .await
+        .map_err(|e| format!("get canister eth addr failed: {}", e))?;
+
+    ic_cdk::println!("canister_addr: {}", canister_addr);
+
+    // add nonce to options
+    let tx_count = w3.eth()
+        .transaction_count(canister_addr, None)
+        .await
+        .map_err(|e| format!("get tx count error: {}", e))?;
+    // get gas_price
+    let gas_price = w3.eth()
+        .gas_price()
+        .await
+        .map_err(|e| format!("get gas_price error: {}", e))?;
+    // legacy transaction type is still ok
+    let options = Options::with(|op| {
+        op.nonce = Some(tx_count);
+        op.gas_price = Some(gas_price);
+        op.transaction_type = Some(ic_web3::ethabi::ethereum_types::U64::from(2)) //EIP1559_TX_ID
+    });
+
+    ic_cdk::println!("Price from oracle: {}", price);
+
+    let txhash = contract
+        .signed_call(&method, (price.to_string(),), options, hex::encode(canister_addr), key_info, CHAIN_ID)
+        .await
+        .map_err(|e| format!("token transfer failed: {}", e))?;
+
+    ic_cdk::println!("txhash: {}", hex::encode(txhash));
+
+    Ok(format!("{}", hex::encode(txhash)))
 }

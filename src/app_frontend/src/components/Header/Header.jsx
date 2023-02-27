5e332353c5e1b50605a3b126d8d7b97760ad8802
Verkhohliad <ihor.verkhohliad@gmail.com>
2023-02-27 09:11:04 +0000

    Front: create flow for subscription with ABI file, update styles

--- src/app_frontend/src/components/Header/Header.jsx
+++ src/app_frontend/src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import Connect from 'Shared/Connect';
+import logoSrc from 'Assets/logo.png';
 
 import styles from './Header.scss';
 
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div className={styles.logo}>ORALLY</div>
+      <div className={styles.logo}>
+        <img src={logoSrc} className={styles.logoImg} alt="Orally" />
+      </div>
 
       <Connect className={styles.connect} />
     </div>

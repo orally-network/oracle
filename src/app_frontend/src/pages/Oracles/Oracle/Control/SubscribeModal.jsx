649d4c27c7f4c172e0aa46ca848e491ed0161e60
Verkhohliad <ihor.verkhohliad@gmail.com>
2023-02-27 09:11:04 +0000

    Front: create flow for subscription with ABI file, update styles

--- src/app_frontend/src/pages/Oracles/Oracle/Control/SubscribeModal.jsx
+++ src/app_frontend/src/pages/Oracles/Oracle/Control/SubscribeModal.jsx
@@ -6,22 +6,36 @@ import Modal from 'Components/Modal';
 import Input from 'Components/Input';
 import Button from 'Components/Button';
 import logger from 'Utils/logger';
+import { remove0x } from 'Utils/addressUtils';
 
 import styles from './Control.scss';
 
 const fileTypes = ["JSON"];
 
 const SubscribeModal = ({ isSubscribeModalOpen, setIsSubscribeModalOpen, addressData, oracle }) => {
+  const [abi, setAbi] = useState(null);
   const [destinationAddress, setDestinationAddress] = useState('');
   const [methodName, setMethodName] = useState('');
   
-  console.log({ oracle });
+  const setFile = useCallback((file) => {
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+      const bytes = e.target.result;
+
+      setAbi(new Uint8Array(bytes));
+    }
+
+    reader.readAsArrayBuffer(file);
+  }, []);
 
   const subscribe = useCallback(async () => {
     setIsSubscribeModalOpen(false);
     
+    console.log({ destinationAddress, methodName, abi: [...abi], addressData })
+    
     const res = await toast.promise(
-      oracle.subscribe(destinationAddress, methodName, addressData.message, addressData.signature),
+      oracle.subscribe(destinationAddress, methodName, [...abi], addressData.message, remove0x(addressData.signature)),
       {
         pending: `Subscribe ${destinationAddress}:${methodName} to oracle`,
         success: `Subscribed successfully`,
@@ -36,7 +50,11 @@ const SubscribeModal = ({ isSubscribeModalOpen, setIsSubscribeModalOpen, address
     );
     
     console.log({ res });
-  }, [methodName, destinationAddress, addressData]);
+    
+    setFile(null);
+    setDestinationAddress('');
+    setMethodName('');
+  }, [methodName, destinationAddress, addressData, abi]);
 
   return (
     <Modal
@@ -44,14 +62,26 @@ const SubscribeModal = ({ isSubscribeModalOpen, setIsSubscribeModalOpen, address
       onClose={useCallback(() => setIsSubscribeModalOpen(false), [])}
     >
       <div className={styles.subscribeModal}>
+        <FileUploader
+          hanldeChange={setFile}
+          name="file"
+          types={fileTypes}
+          label="Upload ABI"
+          onSizeError={() => toast.error('File size is too big')}
+          onDrop={setFile}
+          onSelect={setFile}
+          maxSize={1}
+        />
+        
         <Input
+          className={styles.input}
           value={destinationAddress}
           placeholder="0x..."
           onChange={useCallback((e) => setDestinationAddress(e.target.value), [])}
         />
 
         <Input
-          className={styles.methodInput}
+          className={styles.input}
           value={methodName}
           placeholder="method_name"
           onChange={useCallback((e) => setMethodName(e.target.value), [])}

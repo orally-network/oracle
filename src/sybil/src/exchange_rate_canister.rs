f4c597dd148a4adf86f44340b71aabedecf09fcd
Ihor Verkhohliad <ihor.verkhohliad@gmail.com>
2023-04-25 16:30:42 +0300

    Sibyl V1  (#2)
    * integrate exchange_rate_canister
    * implement asset_data_store with merkle tree and hashMap of asset data
    * implement common asset fetcher with usage of exchange_rate_canister
    * add EXCHANGE_RATE_CANISTER_PRINCIPAL as state of canister with getter and setter
    * merkle tree verification data,
    * providing root hash to destination chain,
    * provide data and proof for that data for attaching it to transaction and verify on end of DeFi contract logic
    * timer for refreshing data in asset data store

---
+++ src/sybil/src/exchange_rate_canister.rs
@@ -0,0 +1,76 @@
+use ic_cdk::export::{
+    candid::{CandidType, Deserialize},
+};
+use ic_cdk::api::call::CallResult;
+use ic_cdk::api::call::{call_with_payment};
+
+use crate::*;
+
+pub const CYCLES_TO_SEND: u64 = 10_000_000_000;
+
+#[derive(CandidType, Deserialize, Debug)]
+pub enum AssetClass { Cryptocurrency, FiatCurrency }
+
+#[derive(CandidType, Deserialize, Debug)]
+pub struct Asset { pub(crate) class: AssetClass, pub(crate) symbol: String }
+
+#[derive(CandidType, Deserialize, Debug)]
+pub struct GetExchangeRateRequest {
+    pub(crate) timestamp: Option<u64>,
+    pub(crate) quote_asset: Asset,
+    pub(crate) base_asset: Asset,
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+pub struct ExchangeRateMetadata {
+    pub decimals: u32,
+    forex_timestamp: Option<u64>,
+    quote_asset_num_received_rates: u64,
+    pub base_asset_num_received_rates: u64,
+    pub base_asset_num_queried_sources: u64,
+    standard_deviation: u64,
+    quote_asset_num_queried_sources: u64,
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+pub struct ExchangeRate {
+    pub metadata: ExchangeRateMetadata,
+    pub(crate) rate: u64,
+    pub timestamp: u64,
+    quote_asset: Asset,
+    base_asset: Asset,
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+pub enum ExchangeRateError {
+    AnonymousPrincipalNotAllowed,
+    CryptoQuoteAssetNotFound,
+    FailedToAcceptCycles,
+    ForexBaseAssetNotFound,
+    CryptoBaseAssetNotFound,
+    StablecoinRateTooFewRates,
+    ForexAssetsNotFound,
+    InconsistentRatesReceived,
+    RateLimited,
+    StablecoinRateZeroRate,
+    Other{ code: u32, description: String },
+    ForexInvalidTimestamp,
+    NotEnoughCycles,
+    ForexQuoteAssetNotFound,
+    StablecoinRateNotFound,
+    Pending,
+}
+
+#[derive(CandidType, Deserialize, Debug)]
+pub enum GetExchangeRateResult { Ok(ExchangeRate), Err(ExchangeRateError) }
+
+pub struct SERVICE(pub Principal);
+
+impl SERVICE{
+    pub async fn get_exchange_rate(
+        &self,
+        arg0: GetExchangeRateRequest,
+    ) -> CallResult<(GetExchangeRateResult,)> {
+        call_with_payment(self.0, "get_exchange_rate", (arg0,), CYCLES_TO_SEND).await
+    }
+}
---
+++ src/sybil/src/main.rs
@@ -0,0 +1,68 @@
+
+use ic_cdk::export::{
+    candid,
+    Principal,
+    serde::{Deserialize, Serialize},
+    candid::CandidType,
+};
+use ic_cdk::{
+    timer::{clear_timer, set_timer_interval, TimerId},
+};
+use ic_cdk_macros::{self, update, query, init, import, pre_upgrade, post_upgrade};
+use std::collections::HashMap;
+
+use ic_web3::{
+    contract::{Contract, Options},
+    ethabi::ethereum_types::{U64, U256},
+    types::{Address, TransactionParameters, BlockId, BlockNumber},
+    transports::{ICHttp},
+    Web3,
+    ic::{get_eth_addr, KeyInfo},
+    futures::TryFutureExt,
+};
+use std::str::FromStr;
+use tiny_keccak::{Hasher, Keccak};
+
+use std::cell::{RefCell};
+use std::time::Duration;
+
+use types::{Chain, Chains, Pair, Pairs, CustomPair, CustomPairs, RateData};
+use state::{STATE, State, };
+// use timer::{run_timer};
+use asset_data_store::{AssetDataStore, AssetData};
+mod types;
+
+mod price_fetcher;
+mod evm_interaction;
+mod exchange_rate_canister;
+mod asset_data_store;
+mod state;
+mod migration;
+mod timer;
+mod processing;
+mod merkle_tree;
+mod bytesc;
+
+pub fn keccak256<T: AsRef<[u8]>>(bytes: T) -> [u8; 32] {
+    let mut output = [0u8; 32];
+    
+    let mut hasher = Keccak::v256();
+    hasher.update(bytes.as_ref());
+    hasher.finalize(&mut output);
+    
+    output
+}
+
+#[init]
+fn init() {
+    ic_cdk::println!("Initializing sybil");
+    canistergeek_ic_rust::logger::log_message("Initializing sybil".to_string());
+    STATE.with(|state| {
+        *state.borrow_mut() = State::new();
+    });
+}
+
+fn main() {
+    candid::export_service!();
+    std::print!("{}", __export_service());
+}

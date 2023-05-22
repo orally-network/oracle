4e0930b07a005a4eb7695b5824bb2774a08b8d44
Verkhohliad <ihor.verkhohliad@gmail.com>
2023-05-22 15:41:00 +0100

    contract_example: update contract example for testing price, randomness and empty automation

--- src/oracle/src/contracts/icp_price.sol
+++ src/oracle/src/contracts/icp_price.sol
@@ -2,15 +2,20 @@
 pragma solidity >=0.8.7;
 
 contract Counter {
-    string public price;
-    bytes32 public random;
+    uint64 public price;
+    uint64 public random;
+    uint64 public counter = 0;
 
-    function set_price(string memory _price) public {
+    function set_price(uint64 _price) public {
         price = _price;
     }
 
-    function set_random(bytes32 _random) public {
+    function set_random(uint64 _random) public {
         random = _random;
+    }
+
+    function increment_counter() public {
+        counter++;
     }
 }
 

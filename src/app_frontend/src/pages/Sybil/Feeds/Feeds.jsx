687967bdb48164596af70ecbc77306a5669bc44c
Andrii Romanenko <andrii.romanenko@holovis.com>
2023-06-12 13:15:48 +0200

    mobile layout fixes

--- src/app_frontend/src/pages/Sybil/Feeds/Feeds.jsx
+++ src/app_frontend/src/pages/Sybil/Feeds/Feeds.jsx
@@ -1,59 +1,68 @@
-import React, { useState, useCallback, useMemo, useEffect } from 'react';
-import { Card } from 'antd';
-import cn from 'classnames';
-import { CopyBlock, atomOneLight } from 'react-code-blocks';
-import { Spin } from 'antd';
+import React, { useState, useCallback, useMemo, useEffect } from "react";
+import { Card } from "antd";
+import cn from "classnames";
+import { CopyBlock, atomOneLight } from "react-code-blocks";
+import { Spin } from "antd";
 
-import config from 'Constants/config';
-import { useSybilPairs } from 'Providers/SybilPairs';
+import config from "Constants/config";
+import { useSybilPairs } from "Providers/SybilPairs";
 
-import styles from './Feeds.scss';
+import styles from "./Feeds.scss";
 
 const MILLI = Math.pow(10, 3);
 
 const getCodeText = (selectedFeed) => {
-  const url = `http${config.isDevelopment ? '' : 's'}://${config.sybil_canister_id}.${config.DOMAIN}/get_asset_data_with_proof?pair_id=${selectedFeed?.id ?? '{pair_id}'}`;
-  
+  const url = `http${config.isDevelopment ? "" : "s"}://${
+    config.sybil_canister_id
+  }.${config.DOMAIN}/get_asset_data_with_proof?pair_id=${
+    selectedFeed?.id ?? "{pair_id}"
+  }`;
+
   return ` // fetch price feed
   const url = '${url}';
   const response = await fetch(url);
-  
+
   // response example:
   {
-    "symbol": "${selectedFeed?.id ?? 'string'}",
-    "rate": ${selectedFeed?.data?.rate ?? 'number'},
-    "timestamp": ${selectedFeed?.data?.timestamp ?? 'number'}, // ${new Date(Number(selectedFeed?.data?.timestamp) * MILLI).toGMTString()}
-    "decimals": ${selectedFeed?.data?.decimals ?? 'number'}
-    "signature": "${selectedFeed?.data?.signature ?? 'string'}"
+    "symbol": "${selectedFeed?.id ?? "string"}",
+    "rate": ${selectedFeed?.data?.rate ?? "number"},
+    "timestamp": ${selectedFeed?.data?.timestamp ?? "number"}, // ${new Date(
+    Number(selectedFeed?.data?.timestamp) * MILLI
+  ).toGMTString()}
+    "decimals": ${selectedFeed?.data?.decimals ?? "number"}
+    "signature": "${selectedFeed?.data?.signature ?? "string"}"
   }
   `;
-}
+};
 
 const Feeds = () => {
   const { pairs, isLoading } = useSybilPairs();
   const [selectedFeed, setSelectedFeed] = useState(null);
-  
+
   useEffect(() => {
     if (pairs.length > 0) {
       setSelectedFeed(pairs[0]);
     }
   }, [pairs]);
-  
+
   const codeText = useMemo(() => {
     return getCodeText(selectedFeed);
   }, [selectedFeed]);
-  
+
   const handleFeedClick = useCallback((feed) => {
     setSelectedFeed(feed);
   }, []);
-  
+
   return (
     <div className={styles.feedsWrapper}>
       <Spin spinning={isLoading}>
         <div className={styles.feeds}>
           {pairs.map((feed) => (
             <Card
-              className={cn([styles.feed, feed.id === selectedFeed?.id && styles.selected])}
+              className={cn([
+                styles.feed,
+                feed.id === selectedFeed?.id && styles.selected,
+              ])}
               key={feed.id}
               onClick={handleFeedClick.bind(null, feed)}
             >
@@ -67,14 +76,14 @@ const Feeds = () => {
         <div className={styles.feedDetails}>
           <CopyBlock
             text={codeText}
-            language={'typescript'}
+            language={"typescript"}
             theme={atomOneLight}
             codeBlock
           />
         </div>
       </Spin>
     </div>
-  )
+  );
 };
 
 export default Feeds;

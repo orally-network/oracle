e1ba4e42a34aff6859410394d992851589a3271d
Nadiia Balaian <--global>
2023-11-07 18:27:48 +0000

    add wallet connect

--- src/app_frontend/src/shared/Connect/Connect.jsx
+++ src/app_frontend/src/shared/Connect/Connect.jsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import { useAccount, useConnect, useDisconnect } from 'wagmi';
-import { InjectedConnector } from 'wagmi/connectors/injected';
+import React from "react";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
 
-import Button from 'Components/Button';
-import { truncateEthAddress } from 'Utils/addressUtils';
+import Button from "Components/Button";
+import { truncateEthAddress } from "Utils/addressUtils";
 
 const Connect = ({ className }) => {
-  const { address, isConnected } = useAccount()
-  const { connect } = useConnect({
-    connector: new InjectedConnector(),
-  })
-  const { disconnect } = useDisconnect()
+  const { open, close } = useWeb3Modal();
 
-  return (
-    <Button
-      className={className}
-      onClick={() => isConnected ? disconnect() : connect()}
-    >
-      {isConnected ? truncateEthAddress(address) : 'Connect'}
-    </Button>
-  );
+  return <w3m-button />;
 };
 
 export default Connect;

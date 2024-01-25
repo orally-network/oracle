45131f78c8e3e5c5f20a63c5499b3480ab273ad0
verkhohliad <ihor.verkhohliad@gmail.com>
2024-01-25 10:56:16 +0000

    change default page size for requesting feeds and subscrptions

--- src/app_frontend/src/constants/ui.ts
+++ src/app_frontend/src/constants/ui.ts
@@ -10,8 +10,8 @@ export const MIN_FREQUENCY = 1800;
 export const MIN_BALANCE = 0.1;
 export const EMPTY_BALANCE = 0.001;
 
-export const DEFAULT_SUBSCRIPTIONS = 10;
-export const DEFAULT_FEEDS_SIZE = 10;
+export const DEFAULT_SUBSCRIPTIONS = 30;
+export const DEFAULT_FEEDS_SIZE = 30;
 export const DEFAULT_TOP_UP_AMOUNT = 0.01;
 export const DEFAULT_SUBSCRIPTIONS_SIZE = 10;
 export const MAX_SOURCES = 10;

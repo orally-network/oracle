20be565a24142d65c3c385d3abb589f20c9f8a22
Nadiia Balaian <--global>
2024-01-30 08:35:00 -0500

    add toasts for withdraw

--- src/app_frontend/src/pages/WeatherAuction/Actions.tsx
+++ src/app_frontend/src/pages/WeatherAuction/Actions.tsx
@@ -1,7 +1,8 @@
-import { Button, Flex } from 'antd';
 import React from 'react';
 import styles from './WeatherAuction.scss';
 import { useWeatherData } from 'Providers/WeatherAuctionData/useWeatherData';
+import { Flex } from 'antd';
+import Button from 'Components/Button';
 
 export const Actions = () => {
   const { withdraw, userWinningBalance } = useWeatherData();
@@ -12,7 +13,7 @@ export const Actions = () => {
         <span>My winnings</span>
         <span className={styles.accentText}>{userWinningBalance} ETH</span>
       </Flex>
-      <Button type="primary" onClick={withdraw}>
+      <Button type="primary" onClick={withdraw} disabled={userWinningBalance === 0}>
         Withdraw
       </Button>
     </Flex>
